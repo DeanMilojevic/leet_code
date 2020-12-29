4b7fb9dc19d49c59e7c20afbbb32172a2d4357ad
Dean Milojevic <milojevicdean@gmail.com>
2020-12-29 11:10:13 +0100

    Updated formatting

--- Go/zig_zag_conversion/solution.go
+++ Go/zig_zag_conversion/solution.go
@@ -19,15 +19,22 @@ func convert(s string, numRows int) string {
 
 	var arr = make([]string, numRows)
 	row := 0
+	down := false
 
 	for i := 0; i < len(s); i++ {
 		arr[row] = arr[row] + s[i:i+1]
 
 		if row == numRows-1 {
-			row++
+			down = false
 		}
 
 		if row == 0 {
+			down = true
+		}
+
+		if down {
+			row++
+		} else {
 			row--
 		}
 	}
