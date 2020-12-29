86377203eaa76e0c6c655f51da3df1dab64b5a31
Dean Milojevic <milojevicdean@gmail.com>
2020-12-29 13:50:29 +0100

    Solution for the String to Integer (atoi) problem

--- Go/string_to_integer_atoi/solution.go
+++ Go/string_to_integer_atoi/solution.go
@@ -1,7 +1,57 @@
 package main
 
+import (
+	"math"
+)
+
 func main() {}
 
 func myAtoi(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
 
+	whitespace := byte(' ')
+	minus := byte('-')
+	plus := byte('+')
+	zero := byte('0')
+	nine := byte('9')
+
+	result := 0
+	sign := 1
+
+	index := 0
+
+	for index < len(s) && s[index] == whitespace {
+		index++
+	}
+
+	if index < len(s) && (s[index] == plus || s[index] == minus) {
+		if s[index] == minus {
+			sign = -1
+			index++
+		} else if s[index] == plus {
+			sign = 1
+			index++
+		}
+	}
+
+	for index < len(s) && s[index] >= zero && s[index] <= nine {
+		if result > math.MaxInt32/10 || (result == math.MaxInt32/10 && s[index]-'0' > math.MaxInt32%10) {
+			if sign == 1 {
+				return math.MaxInt32
+			}
+
+			if sign == -1 {
+				return math.MinInt32
+			}
+		}
+
+		result = result*10 + int(s[index]-'0')
+		index++
+	}
+
+	result = result * sign
+
+	return result
 }
--- Go/string_to_integer_atoi/solution_test.go
+++ Go/string_to_integer_atoi/solution_test.go
@@ -41,3 +41,43 @@ func Test05(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func Test06(t *testing.T) {
+	result := myAtoi("3.14159")
+
+	if result != 3 {
+		t.Fail()
+	}
+}
+
+func Test07(t *testing.T) {
+	result := myAtoi("00000-42a1234")
+
+	if result != 0 {
+		t.Fail()
+	}
+}
+
+func Test08(t *testing.T) {
+	result := myAtoi("+-12")
+
+	if result != 0 {
+		t.Fail()
+	}
+}
+
+func Test09(t *testing.T) {
+	result := myAtoi("-+12")
+
+	if result != 0 {
+		t.Fail()
+	}
+}
+
+func Test10(t *testing.T) {
+	result := myAtoi("9223372036854775808")
+
+	if result != 2147483647 {
+		t.Fail()
+	}
+}
